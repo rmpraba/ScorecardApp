747a0674188514c74a9c3ccc76e5ab89e7a20f50
KartikRajaS <kartiksubu.krs@gmail.com>
2016-05-11 15:46:26 +0530

    email aligned

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ var parentemail=req.query.parentemail;
   var bank=req.query.bank;
 console.log(parentemail);
 var server  = email.server.connect({
-   user:    "samsidhgroup@yahoo.com",
-   password:"mlzsinstitutions",
+   user:    "kartiksubu.krs@yahoo.com",
+   password:"kartik2903",
    host:    "smtp.mail.yahoo.com",
    ssl:     true
 
@@ -44,24 +44,23 @@ var server  = email.server.connect({
 // send the message and get a callback with an error or details of the message that was sent
 server.send({
    text:    "FEE RECEIPT/ACKNOWLEDGEMENT",
-   from:    "samsidhgroup@yahoo.com",
-   to:       parentemail,
+   from:    "kartiksubu.krs@yahoo.com",
+   to:      "kartiksubu.krs@gmail.com",
    subject: "FEE RECEIPT/ACKNOWLEDGEMENT",
     attachment:
 
       [
         {data:"<html><body class='receipt'><table><caption style='text-align: center;'><strong>FEE "+receipttitle+"</strong></caption></caption>" +
-        "<tr><td class='left1'><strong>Receipt No : </strong>"+receiptno+"</td><td style='width: 110px;' class='center1'></td><td class='right1'><strong>Receipt Date : </strong>"+today+"</td></tr>" +
+        "<tr><td class='left1' style='width: 230px;'><strong>Receipt No : </strong>"+receiptno+"</td><td style='width: 42px;' class='center1'></td><td style='width: 170px;' class='right1'><strong>Receipt Date : </strong>"+today+"</td></tr>" +
         "<tr><td class='left1'><strong>Student Name : </strong>"+studname+"</td><td class='center1'></td><td class='right1'><strong>Class : </strong>"+classname+"</td></tr>" +
         "<tr><td class='left1'><strong>Parent Name : </strong>"+parentname+"</td><td class='center1'></td><td class='right1'><strong>Session : </strong>"+session+"</td></tr></table>" +
-        "<table><tr><td style='width: 50px; text-align: center;' class='left2'><strong>SL.No</strong></td><td style='width: 280px; text-align: center;' class='center2'><strong>Particulars</strong></td><td style='text-align: right;' class='right2'><strong>Amount</strong></td></tr>" +
+        "<table><tr><td style='width: 75px; text-align: center;' class='left2'><strong>SL.No</strong></td><td style='width: 276px; text-align: center;' class='center2'><strong>Particulars</strong></td><td style='text-align: right;' class='right2'><strong>Amount</strong></td></tr>" +
         "<tr><td class='left2' style='text-align: center;'>1.</td><td class='center2' style='text-align: center;'><strong>Transportfee : </strong>"+installtype+"</td><td class='right2'style='text-align: center;'>"+installfee+"</td></tr></table>" +
-        "<table><tr><td class='left3'><strong>In Words : </strong>"+words+"</td><td class='right3'><strong>Total :</strong>"+installfee+"</td></tr>" +
+        "<table><tr><td class='left3' style='width: 323px;'><strong>In Words : </strong>"+words+"</td><td class='right3'><strong>Total :</strong>"+installfee+"</td></tr>" +
         "<tr><td class='left3'><strong>Mode of Payments : </strong>"+mode+"</td><td class='right3'></td></tr></table>" +
-        "<table><tr><td class='left3'><strong>Chequeno : </strong>"+chequeno+"</td><td class='right3'><strong>Chequedate :</strong>"+chequedate+"</td></tr>" +
-        "<tr><td class='left3'><strong>Bank name : </strong>"+bank+"</td><td class='right3'></td></tr></table>" +
-        "<div class='terms'><div class='head'><strong><U>TERMS AND CONDITIONS</U></strong></div>" +
-        "<div class='body'><p>1.  In Case the cheque is not honoured by the bank, service charge of Rs.250/- will <br>be levied and the amount has to be paid by Cash / DD.</p>" +
+        "<table><tr><td class='left3'><strong>Chequeno : </strong>"+chequeno+"</td><td class='right3'><strong>Chequedate :</strong>"+chequedate+"</td><td class='rytls'><strong>Bank name : </strong>"+bank+"</td><td class='right3'></td></tr></table>" +
+        "<div class='terms'><div style='text-align: center;' class='head'><strong><U>TERMS AND CONDITIONS</U></strong></div>" +
+        "<div class='body'><p>1.  In Case the cheque is not honoured by the bank, service charge of Rs.250/- <br>will be levied and the amount has to be paid by Cash / DD.</p>" +
         "<p>2.  Fees once paid will not be refunded at any given circumstances.</p>" +
         "<p>3.  Cheque Subject to realization.</p>" +
         "<p>4.  Please retain this receipt for future correspondence.</p></div>" +
@@ -2098,7 +2097,7 @@ app.post('/getfeecheque',  urlencodedParser,function (req, res)
 {
  var id=req.query.stid;
  console.log(id);
-	
+
 	    connection.query('Select * from cheque_details where student_id=?',[id],
        	function(err, rows){
 		if(!err){

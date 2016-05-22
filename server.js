2067db9b415c50e6d924af93b0c66c3c14676f2a
Dhivakar Saravanan <dhivakarksaravanan@gmail.com>
2016-05-22 17:34:39 +0530

    values for report

--- server.js
+++ server.js
@@ -3442,6 +3442,129 @@ app.post('/consolidatedallocatedreportread',  urlencodedParser,function (req, re
 	});
 });
 
+app.post('/valuesinsta1cheque',  urlencodedParser,function (req, res)
+{
+	var date={"installment_1Date":req.query.dates};
+	var mode= {"modeofpayment1":"Cheque"};
+	var schoolx=req.query.schol;
+	    connection.query('select f.student_id, f.student_id, d.student_name, f.receipt_no1, f.fees,f.installment_1, c.cheque_no, c.bank_name, c.cheque_date from student_fee f inner join student_details d on f.student_id = d.id inner join cheque_details c on f.student_id = c.student_id where ? and ? and d.school_id=?',[date, mode,schoolx],
+       	function(err, rows){
+       		var itemarr = new Array();
+       		if(!err){
+		       	if(rows.length>0){
+			        for(var i=0;i<rows.length;i++){
+			            var obj={"student_id":"","student_name":"","receipt_no":"","fees":"","installment_1":"","cheque_no":"","bank_name":"","cheque_date":"",};
+			            obj.student_id=rows[i].student_id;
+			            obj.student_name=rows[i].student_name;
+			            obj.receipt_no=rows[i].receipt_no;
+			            obj.fees=rows[i].fees;
+			            obj.installment_1=rows[i].installment_1;
+			            obj.cheque_no=rows[i].cheque_no;
+			            obj.bank_name=rows[i].bank_name;
+			            obj.cheque_date=rows[i].cheque_date;
+			            itemarr.push(obj);
+			        }
+		           	//console.log(JSON.stringify(itemarr));
+					res.status(200).json({'returnval': itemarr});
+		        } else {
+		            res.status(200).json({'returnval': ''});
+		        }
+	        } else {
+	          console.log(err);
+	        }
+	});
+});
+
+app.post('/valuesinsta2cheque',  urlencodedParser,function (req, res)
+{
+	var date={"installment_2Date":req.query.dates};
+	var mode= {"modeofpayment2":"Cheque"};
+	var schoolx=req.query.schol;
+	    connection.query('select f.student_id, f.student_id, d.student_name, f.receipt_no2, f.fees,f.installment_2, c.cheque_no, c.bank_name, c.cheque_date from student_fee f inner join student_details d on f.student_id = d.id inner join cheque_details c on f.student_id = c.student_id where ? and ? and d.school_id=?',[date, mode,schoolx],
+       	function(err, rows){
+       		var itemarr = new Array();
+       		if(!err){
+		       	if(rows.length>0){
+			        for(var i=0;i<rows.length;i++){
+			            var obj={"student_id":"","student_name":"","receipt_no":"","fees":"","installment_1":"","cheque_no":"","bank_name":"","cheque_date":"",};
+			            obj.student_id=rows[i].student_id;
+			            obj.student_name=rows[i].student_name;
+			            obj.receipt_no=rows[i].receipt_no;
+			            obj.fees=rows[i].fees;
+			            obj.installment_1=rows[i].installment_2;
+			            obj.cheque_no=rows[i].cheque_no;
+			            obj.bank_name=rows[i].bank_name;
+			            obj.cheque_date=rows[i].cheque_date;
+			            itemarr.push(obj);
+			        }
+		           	//console.log(JSON.stringify(itemarr));
+					res.status(200).json({'returnval': itemarr});
+		        } else {
+		            res.status(200).json({'returnval': ''});
+		        }
+	        } else {
+	          console.log(err);
+	        }
+	});
+});
+app.post('/valuesinsta1cash',  urlencodedParser,function (req, res)
+{
+	var date={"installment_1Date":req.query.dates};
+	var mode= {"modeofpayment1":"Cash"};
+	var schoolx=req.query.schol;
+	    connection.query('Select f.student_id, d.student_name, f.receipt_no1, f.fees,f.installment_1 from student_fee f left join student_details d on f.student_id=d.id where ? and ? and d.school_id=?',[date, mode,schoolx],
+       	function(err, rows){
+       		var itemarr = new Array();
+       		if(!err){
+		       	if(rows.length>0){
+			        for(var i=0;i<rows.length;i++){
+			            var obj={"student_id":"","student_name":"","receipt_no":"","fees":"","installment_1":""};
+			            obj.student_id=rows[i].student_id;
+			            obj.student_name=rows[i].student_name;
+			            obj.receipt_no=rows[i].receipt_no;
+			            obj.fees=rows[i].fees;
+			            obj.installment_1=rows[i].installment_1;
+			            itemarr.push(obj);
+			        }
+		           	//console.log(JSON.stringify(itemarr));
+					res.status(200).json({'returnval': itemarr});
+		        } else {
+		            res.status(200).json({'returnval': ''});
+		        }
+	        } else {
+	          console.log(err);
+	        }
+	});
+});  
+app.post('/valuesinsta2cash',  urlencodedParser,function (req, res)
+{
+	var date={"installment_2Date":req.query.dates};
+	var mode= {"modeofpayment1":"Cash"};
+	var schoolx=req.query.schol;
+	    connection.query('Select f.student_id, d.student_name, f.receipt_no2, f.fees,f.installment_2 from student_fee f left join student_details d on f.student_id=d.id where ? and ? and d.school_id=?',[date, mode,schoolx],
+       	function(err, rows){
+       		var itemarr = new Array();
+       		if(!err){
+		       	if(rows.length>0){
+			        for(var i=0;i<rows.length;i++){
+			            var obj={"student_id":"","student_name":"","receipt_no":"","fees":"","installment_1":""};
+			            obj.student_id=rows[i].student_id;
+			            obj.student_name=rows[i].student_name;
+			            obj.receipt_no=rows[i].receipt_no;
+			            obj.fees=rows[i].fees;
+			            obj.installment_1=rows[i].installment_2;
+			            itemarr.push(obj);
+			        }
+		           	//console.log(JSON.stringify(itemarr));
+					res.status(200).json({'returnval': itemarr});
+		        } else {
+		            res.status(200).json({'returnval': ''});
+		        }
+	        } else {
+	          console.log(err);
+	        }
+	});
+});
 function setvalue(){
 	console.log("calling setvalue.....");
 }
@@ -3449,4 +3572,4 @@ var server = app.listen(8081, function () {
 var host = server.address().address
 var port = server.address().port
 console.log("Example app listening at http://%s:%s", host, port)
-})
+});

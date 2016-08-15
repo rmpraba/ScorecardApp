4c3e0809ca53c189027f6a094c88827662bddeac
rmpraba <rmpraba@gmail.com>
2016-08-15 12:35:07 +0530

    score card changes

--- server.js
+++ server.js
@@ -1414,14 +1414,25 @@ app.post('/fetchworkingdays-service',  urlencodedParser,function (req, res)
 
 app.post('/updateimportmarkcheck-service' ,  urlencodedParser,function (req, res)
 {
-  var qur="SELECT CASE WHEN count1 = count2 THEN 'match' ELSE 'mismatch' END as result FROM(SELECT "+
+var qur;
+if(req.query.subject=='Hindi'||req.query.subject=='Kannada'){
+qur="SELECT CASE WHEN count1 = count2 THEN 'match' ELSE 'mismatch' END as result FROM(SELECT "+
+"(select count(distinct(student_id)) from tr_term_assesment_marks "+
+"where school_id='"+req.query.schoolid+"' and grade='"+req.query.gradename+"' and section='"+req.query.sectionname+"' "+
+"and subject_id='"+req.query.subject+"' and term_name='"+req.query.termname+"' and assesment_id='"+req.query.assesmentid+"') AS count1, "+
+"(select count(*) from tr_student_to_subject where school_id='"+req.query.schoolid+"' and class_id=(select class_id from mp_grade_section where grade_id=(select grade_id "+
+"from md_grade where grade_name='"+req.query.gradename+"') and section_id=(select "+
+"section_id from md_section where section_name='"+req.query.sectionname+"' and subject_name='"+req.query.subject+"' and school_id='"+req.query.schoolid+"'))) AS count2)  AS counts";
+}
+else{
+qur="SELECT CASE WHEN count1 = count2 THEN 'match' ELSE 'mismatch' END as result FROM(SELECT "+
 "(select count(distinct(student_id)) from tr_term_assesment_marks "+
 "where school_id='"+req.query.schoolid+"' and grade='"+req.query.gradename+"' and section='"+req.query.sectionname+"' "+
 "and subject_id='"+req.query.subject+"' and term_name='"+req.query.termname+"' and assesment_id='"+req.query.assesmentid+"') AS count1, "+
 "(select count(*) from md_student where school_id='"+req.query.schoolid+"' and class_id=(select class_id from mp_grade_section where grade_id=(select grade_id "+
 "from md_grade where grade_name='"+req.query.gradename+"') and section_id=(select "+
 "section_id from md_section where section_name='"+req.query.sectionname+"' and school_id='"+req.query.schoolid+"'))) AS count2)  AS counts";
-  
+}
 console.log('----------------------------------------------------------');
 console.log(qur);
   connection.query(qur,

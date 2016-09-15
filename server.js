fd461fbc90f4012e53cfc6c237f025db5a5629cb
tamils30 <ntamilselvimca@gmail.com>
2016-09-15 12:14:25 +0530

    sa -import mark

--- server.js
+++ server.js
@@ -1932,7 +1932,7 @@ app.post('/fetchstudentreport-service',  urlencodedParser,function (req, res)
 app.post('/fetchfastudentreport-service',  urlencodedParser,function (req, res)
 {
 
-  var qur="select * from tr_term_fa_assesment_marks where  grade='"+req.query.gradename+"' and section ='"+req.query.section+"' and school_id='"+req.query.schoolid+"' and subject_id='"+req.query.subject+"' and assesment_id='"+req.query.assesment+"' and term_name='"+req.query.termname+"'";
+  var qur="select * from tr_term_fa_assesment_marks where  grade='"+req.query.gradename+"' and section ='"+req.query.section+"' and school_id='"+req.query.schoolid+"' and subject_id='"+req.query.subject+"' and assesment_id='"+req.query.assesment+"' and term_name='"+req.query.termname+"' and category='"+req.query.assesmenttype+"'";
   console.log('----------------------------------------fetchreport----------');
   console.log(qur);
   connection.query(qur,
